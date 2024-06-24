b99721ae171c7a039b9b93d0482b104b2b108b63
Qgq <qgq4227@163.com>
2024-06-24 17:22:34 +0800

    封装tooltip组件

---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Tooltip } from './Tooltip';
+import { Fragment } from 'react/jsx-runtime';
+
+const meta = {
+  component: Tooltip,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Tooltip>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: "Tooltip title",
+    placement: "top",
+    children: <button>hover</button>
+  }
+};
---
+++ src/stories/Tooltip.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react"
+import "./tooltip.css"
+import { autoUpdate, flip, offset, shift, useDismiss, useFloating, useHover, useInteractions, useRole, FloatingArrow, arrow } from "@floating-ui/react";
+
+
+
+interface TooltipProps {
+  title: String,
+  placement: | "top-start" | "top-end" | "right-start" | "right-end" | "bottom-start" | "bottom-end" | "left-start" | "left-end",
+  children: React.ReactElement
+}
+
+
+
+export const Tooltip = ({ title, placement, children }: TooltipProps) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const arrowRef = useRef(null);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: placement,
+
+    middleware: [offset(10), flip(), shift(), arrow({ element: arrowRef })],
+    whileElementsMounted: autoUpdate
+  });
+
+  const hover = useHover(context, { move: false });
+  const focus = useHover(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context, {
+    role: "tooltip"
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover, focus, dismiss, role]);
+
+
+
+  return (
+    <>
+      <span className="reference-el-wraper" ref={refs.setReference} {...getReferenceProps()}>
+        {
+          children
+        }
+      </span>
+      {
+        isOpen && (
+          <div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            {...getFloatingProps()}
+            className="tooltip"
+          >
+            {title}
+            <FloatingArrow ref={arrowRef} context={context}></FloatingArrow>
+          </div>
+        )
+      }
+    </>
+  )
+}
