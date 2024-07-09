34faf20abce2d615550c89259b1cbdf3310c0a86
Qgq <qgq4227@163.com>
2024-07-09 21:34:23 +0800

    update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +13,15 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config, { configType }) => {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      build: {
+        outDir: "docs"  // 使得文档打包结果输出到docs目录以激活github pages
+      }
+    });
   },
 };
 export default config;
